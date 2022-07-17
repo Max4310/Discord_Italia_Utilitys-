f0c6d108bc787d903edc8b61fc9e88c26fea669a
Max4310 <93782481+Max4310@users.noreply.github.com>
2022-07-18 00:10:39 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1006,37 +1006,6 @@ client.on("interactionCreate", (comando)=>{
 })
 
 client.on("interactionCreate", (interaction) => {
-    if(interaction.customId.split(",")[0] == "cd_verifica")
-    {
-        try{
-            if(interaction.fields.getTextInputValue("verifica_testo") == interaction.customId.split(",")[1])
-            {
-                interaction.member.roles.add(interaction.guild.roles.cache.get(abitante))
-                interaction.member.roles.add(interaction.guild.roles.cache.get(stella1))
-                interaction.deferUpdate()
-    
-                var message_log = new Discord.MessageEmbed()
-                    .setTitle("DISCORD ITALIA")
-                    .setDescription("<@"+interaction.member.user.id+"> Si è Verificato Con Successo")
-                    .setColor("BLUE")
-    
-                interaction.guild.channels.cache.get(log_verifica).send({embeds : [message_log]})
-            }
-            else
-            {
-                var risposta = new Discord.MessageEmbed()
-                    .setTitle("DISCORD ITALIA")
-                    .setDescription("**Il Codice Risulta Essere Sbagliato**\n\n*Ricordo Che Non Bisogna Mettere Spazi, Ma Scrivere Semplicemente Il Risultato.*")
-                    .setFooter("Contattare L'assistenza In Caso Di Problemi")
-                    .setColor("RED")
-    
-                interaction.reply({embeds : [risposta], ephemeral : true})
-            }
-        }catch{
-            return
-        }
-        
-    }
     if(interaction.commandName == "regole"){
         try{
             const logchannel = interaction.guild.channels.cache.get(log) //log
@@ -1312,6 +1281,37 @@ client.on("interactionCreate", (interaction) => {
         
                 interaction.reply({content : "Qualcosa è Andato Storto", ephemeral : true})
         
+                return
+            }
+            
+        }
+        if(interaction.customId.split(",")[0] == "cd_verifica")
+        {
+            try{
+                if(interaction.fields.getTextInputValue("verifica_testo") == interaction.customId.split(",")[1])
+                {
+                    interaction.member.roles.add(interaction.guild.roles.cache.get(abitante))
+                    interaction.member.roles.add(interaction.guild.roles.cache.get(stella1))
+                    interaction.deferUpdate()
+        
+                    var message_log = new Discord.MessageEmbed()
+                        .setTitle("DISCORD ITALIA")
+                        .setDescription("<@"+interaction.member.user.id+"> Si è Verificato Con Successo")
+                        .setColor("BLUE")
+        
+                    interaction.guild.channels.cache.get(log_verifica).send({embeds : [message_log]})
+                }
+                else
+                {
+                    var risposta = new Discord.MessageEmbed()
+                        .setTitle("DISCORD ITALIA")
+                        .setDescription("**Il Codice Risulta Essere Sbagliato**\n\n*Ricordo Che Non Bisogna Mettere Spazi, Ma Scrivere Semplicemente Il Risultato.*")
+                        .setFooter("Contattare L'assistenza In Caso Di Problemi")
+                        .setColor("RED")
+        
+                    interaction.reply({embeds : [risposta], ephemeral : true})
+                }
+            }catch{
                 return
             }
             
