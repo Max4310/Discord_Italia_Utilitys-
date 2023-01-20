1ad22d771a2a94b297d2e76232068ce6bfc7e0a3
Max4310 <93782481+Max4310@users.noreply.github.com>
2023-01-21 00:55:23 +0100

    warn

--- codici/comandi/coin/warn.js
+++ codici/comandi/coin/warn.js
@@ -8,7 +8,7 @@ function comando(interaction) {
 
     try{
         if (interaction.channelId == variabili.chatWarn) {
-            if (/*interaction.member.permissions.has("ADMINISTRATOR")*/ interaction.member.roles.has()) {
+            if (/*interaction.member.permissions.has("ADMINISTRATOR")*/ interaction.member.roles.cache.some(role => role.id == variabili.Governo)) {
                 user("warn").then((warnati) => {
                     CoinMember(interaction.member.user.id).then(member => {
     
