31ed87c20a4a40cab4a11dd080471f1121d93fa5
Max4310 <93782481+Max4310@users.noreply.github.com>
2023-01-21 21:15:46 +0100

    ciaoo

--- codici/assistenza/ticket/gestione/close.js
+++ codici/assistenza/ticket/gestione/close.js
@@ -2,11 +2,37 @@ const { GOVERNO, JUSTICEID, YAKUZAID, EMOJIMAKER, BIGBRAIN, PROPOSTE, CEMOJI, AS
 const discord = require("discord.js")
 const {MessageActionRow, MessageButton} = require("discord.js");
 const { createTranscript } = require('discord-html-transcripts');
+const path = require("path")
+const variabili = require(path.join(__dirname,"../../../../variabili.json"))
 
 async function close (interaction)
 {
+
   try{
-    if( interaction.member.permissions.has("ADMINISTRATOR") || (interaction.member.roles.cache.some(role => role.id == YAKUZAID) && interaction.channel.parentId == ACQUISTIID) || (interaction.member.roles.cache.some(role => role.id == HELPERID) && interaction.channel.parentId == ASSISTENZAID) || (interaction.member.roles.cache.some(role => role.id == JUSTICEID) && interaction.channel.parentId == RICORSIID))
+    let cosoFrank = [variabili.Cameraman, variabili.Esaminatore, variabili.Creator, variabili.Producer, variabili.C_esteri, variabili.M_esteri]
+
+    /*"Cameraman":"1066410855673114735",
+    "Esaminatore":"1066410881229000825",
+    "Creator":"1066410899340017775",
+    "Producer":"1066410916842852382",
+    "C_esteri":"1066410956810354712",
+    "M_esteri":"1066410973319147601"*/
+
+    console.log(interaction.channel.parentId)
+
+    if( interaction.member.permissions.has("ADMINISTRATOR") || 
+    (interaction.member.roles.cache.some(role => role.id == YAKUZAID) && interaction.channel.parentId == ACQUISTIID) || 
+    (interaction.member.roles.cache.some(role => role.id == HELPERID) && interaction.channel.parentId == ASSISTENZAID) || 
+    (interaction.member.roles.cache.some(role => role.id == JUSTICEID) && interaction.channel.parentId == RICORSIID) ||
+
+    (interaction.member.roles.cache.some(role => role.id == variabili.Cameraman) || 
+    interaction.member.roles.cache.some(role => role.id == variabili.Esaminatore) || 
+    interaction.member.roles.cache.some(role => role.id == variabili.Creator) || 
+    interaction.member.roles.cache.some(role => role.id == variabili.Producer) || 
+    interaction.member.roles.cache.some(role => role.id == variabili.C_esteri) || 
+    interaction.member.roles.cache.some(role => role.id == variabili.M_esteri) )
+    && interaction.channel.parentId == variabili.categoriaAffiliazioni)
+
     {
       var tipo;
       if(interaction.channel.parentId == ASSISTENZAID){
@@ -16,10 +42,15 @@ async function close (interaction)
       } else if(interaction.channel.parentId == ACQUISTIID){
         tipo = "Acquisti"
       }
+      else if(interaction.channel.parentId == variabili.categoriaAffiliazioni){
+        tipo = "Affiliazioni"
+      }
       else
       {
         return interaction.reply({content : "❌ Non Puoi Eseguire In Questo Canale", ephemeral :true})
       }
+
+
       interaction.reply({content : "👍 Il Ticket Verrà Eliminato Tra 5 Secondi" , ephemeral : true})
       const attachment = await createTranscript(interaction.channel,
         {
